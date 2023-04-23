2719f284c4f84ce22788ffe22af4a1c59298151b
Jose <78896687+jv7924@users.noreply.github.com>
2023-04-22 20:25:19 -0700

    Progress towards multiple input control

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,20 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] playerPrefabs;
+    private Vector2 inputVector;
+    private float horizontalInput;
+    private float verticalInput;
 
-    // Start is called before the first frame update
-    void Start()
+    enum GameMode
     {
-        
+        SINGLE,
+        COOP
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private GameMode mode;
 
+    private void Start()
+    {
+        // Temp
+        mode = GameMode.SINGLE;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        SetUpInputVector(SetUpAxis());
+    }
+
+    private void SetUpInputVector(Vector2 direction)
+    {
+        inputVector = direction;
+    }
+
+    public Vector2 GetVec()
+    {
+        return inputVector;
+    }
+
+    // Temp
+    public Vector2 SetUpAxis()
+    {
+        if (mode == GameMode.SINGLE)
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+        }
+        // else if (mode == GameMode.COOP)
+        // {
+        //     horizontalInput = Input.GetAxisRaw("Horizontal Local " + );
+        //     verticalInput = Input.GetAxisRaw("Vertical Local " + );
+        // }
+
+        return new Vector2(horizontalInput, verticalInput);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,32 +7,35 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float playerSpeed = 0;
     private Rigidbody2D playerRB;
     private Vector2 inputVector;
-    private float horizontalInput;
-    private float verticalInput;
+    // [SerializeField] private int playerNum = 1;
+    InputManager manager;
 
-    // Start is called before the first frame update
+    // Temp
+    
+
     void Awake()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        manager = FindObjectOfType<InputManager>();
     }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    
 
     private void FixedUpdate()
     {
+        inputVector = manager.GetVec();
+
         Move();
     }
 
-    private void SetUpInputVector(float x, float y)
-    {
-        inputVector = new Vector2(x, y);
-    }
+    
 
     private void Move() 
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        SetUpInputVector(horizontalInput, verticalInput);
-
         playerRB.velocity = inputVector * playerSpeed;
     }
 }
