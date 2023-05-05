b517d5a49b3e060e574e2cd18a84b27ade4bfb34
Jose <78896687+jv7924@users.noreply.github.com>
2023-05-04 18:12:34 -0700

    yuh
    yuh

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -10,7 +10,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawner", wait, wait/4);
+        InvokeRepeating("Spawner", wait, 3);
     }
 
     // Update is called once per frame
