d63e09381c05500fa4edfeed6f6a33cda42c2ec5
Jose <78896687+jv7924@users.noreply.github.com>
2023-05-09 23:12:49 -0700

    Semi working bound

--- Assets/Scenes/Test/AimTest.cs
+++ Assets/Scenes/Test/AimTest.cs
@@ -10,27 +10,46 @@ public class AimTest : MonoBehaviour
     public GameObject gameObj;
     public Camera mainCam;
     public PlayerController con;
+    private bool outOfBounds = false;
+    public Transform center;
+    int num;
 
     // Start is called before the first frame update
     void Start()
     {
         obj = GetComponent<Rigidbody2D>();
+        num = con.GetPlayerNumber();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (con.GetPlayerNumber() == 2)
+        CheckBounds();
+
+        if (!outOfBounds)
+            Aim();
+    }
+    
+
+    private void CheckBounds()
+    {
+        Vector2 offset = obj.position - (Vector2)center.transform.position;
+        transform.position = (Vector2)center.transform.position + Vector2.ClampMagnitude(offset, 4);
+    }
+
+    private void Aim()
+    {
+        if (num == 2)
         {
             horizontalInput = Input.GetAxisRaw("Aim Horizontal");
             verticalInput = Input.GetAxisRaw("Aim Vertical");
 
             Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
 
-            obj.velocity = inputVector * 10;
+            obj.velocity = inputVector * 50;
         }
 
-        else if (con.GetPlayerNumber() == 1)
+        else if (num == 1)
             obj.position = mainCam.ScreenToWorldPoint(Input.mousePosition);
     }
 }
