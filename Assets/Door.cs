faadc251200a8f7a5d5e159bb4a2427f1c069f70
ivanono156 <74437367+ivanono156@users.noreply.github.com>
2023-05-16 20:26:14 -0700

    Created Door prefab + script
    Created door script that will open the door when the room is cleared.

---
+++ Assets/Door.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    //[SerializeField]
+    //private BoxCollider2D collider;
+
+    private void Start()
+    {
+        // Subscribe to event here.
+    }
+
+    void Update()
+    {
+        // For testing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OpenDoor();
+        }
+    }
+
+    // Event that triggers when all enemies in a room are defeated.
+    private void OpenDoor()
+    {
+        Debug.Log("Opening Door");
+        animator.SetBool("Room Cleared", true);
+        //collider.enabled = false;
+    }
+}
