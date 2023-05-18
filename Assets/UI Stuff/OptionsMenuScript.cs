14fe3e0feff17b3d66c2af9cc149748c07d448f9
maeina <maeina@uci.edu>
2023-05-17 17:29:57 -0700

    Options Menu Popup code dun sun

--- Assets/UI Stuff/OptionsMenuScript.cs
+++ Assets/UI Stuff/OptionsMenuScript.cs
@@ -6,17 +6,27 @@ public class OptionsMenuScript : MonoBehaviour
 {
     public GameObject menu;
     public GameObject overlay;
+    public GameObject backButton;
     // Start is called before the first frame update
     void Start()
     {
         menu.SetActive(false);
         overlay.SetActive(false);
+        backButton.SetActive(false);
     }
 
     public void OpenOptions()
     {
         menu.SetActive(true);
         overlay.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        menu.SetActive(false);
+        overlay.SetActive(false);
+        backButton.SetActive(false);
     }
     
 }
